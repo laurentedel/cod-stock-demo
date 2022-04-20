fb753151658199bab470190762912be909abc2dc
Laurent Edel <laurent.edel@gmail.com>
2022-04-20 18:04:18 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@
 import logging
 from db import Db
 from get_stock_data import upsert_data, get_dataset
-#logging.basicConfig( level=logging.DEBUG)
 model=Db()
 model.drop_stocks_table()
 model.create_stock_table()
